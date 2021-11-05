43913995513e67fab4aa495db7576efd5c66f3c0
Andrew Kealy <andrewkealy1@gmail.com>
2021-11-05 14:06:48 +0000

    Initial Commit

---
+++ MemorizeTakeTwo/MemorizeTakeTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeTakeTwoApp.swift
+//  MemorizeTakeTwo
+//
+//  Created by Andrew Kealy on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeTakeTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

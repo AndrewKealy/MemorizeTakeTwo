8ea2647134174f87a4341af56188d2c379149432
Andrew Kealy <andrewkealy1@gmail.com>
2021-11-20 17:08:03 +0000

    App as at the end of Lecture 5. Added Struct constants to remove magic numbers. Added an Extension to arrays to incorporate a function checking for exclusivity.

--- MemorizeTakeTwo/EmojiMemroyGame.swift
+++ MemorizeTakeTwo/EmojiMemroyGame.swift
@@ -11,125 +11,30 @@ import SwiftUI
 class EmojiMemoryGame: ObservableObject {
    
     
-    static let emojis = ["🐝", "🐛", "🐜", "🪰", "🦋", "🐌", "🐥", "🦞", "🐒", "🦓", "🦏", "🐓", "🦎", "🐪", "🦒", "🦬", "🐂", "🐎", "🐓", "🦨", "🦫", "🦜", "🐁", "🐿"]
-    
-    private var animalTheme: Theme, vehicleTheme: Theme, fruitTheme: Theme, foodTheme: Theme, flagTheme: Theme, activityTheme: Theme = Theme()
-    private var themes: [Theme] = [Theme]()
-    private(set) var currentTheme: Theme
+    private static let emojis = ["🐝", "🐛", "🐜", "🪰", "🦋", "🐌", "🐥", "🦞", "🐒", "🦓", "🦏", "🐓", "🦎", "🐪", "🦒", "🦬", "🐂", "🐎", "🐓", "🦨", "🦫", "🦜", "🐁", "🐿"]
+    typealias Card = MemoryGame<String>.Card
 
-    
-    init() {
-        let flagEmojis = ["🇮🇪", "🏴󠁧󠁢󠁳󠁣󠁴󠁿", "🏴󠁧󠁢󠁷󠁬󠁳󠁿", "🇺🇸", "🇦🇪", "🇹🇷", "🇵🇪", "🇱🇹", "🇱🇻", "🇭🇰", "🇬🇷", "🇬🇱"]
-        let activityEmojis = ["⚽️", "🏀", "🏈", "🥎", "🎾", "🏐", "🎱", "🥊", "🛼", "⛸", "🏏", "🪁"]
-        let animalemojis = ["🐝", "🐛", "🐜", "🪰", "🦋", "🐌", "🐥", "🦞", "🐒", "🦓", "🦏", "🐓", "🦎", "🐪", "🦒", "🦬", "🐂", "🐎", "🐓", "🦨", "🦫", "🦜", "🐁", "🐿"]
-        let vehicleEmojis = ["🚗", "🚕", "🚙", "🚎", "🚚", "🚛", "🚜", "🚲", "🚔", "🚖", "🚘", "✈️"]
-        let fruitEmojis = ["🍏", "🍎", "🍊", "🍌", "🍉", "🍇", "🍓", "🫐", "🍈", "🍒", "🥝", "🍓"]
-        let foodEmojis = ["🥨", "🥐", "🥯", "🧅", "🧄", "🫒", "🥓", "🌭", "🍗", "🍕", "🌮", "🍣"]
+    private static func createMemoryGame() -> MemoryGame<String> {
         
-        let themeNumbersOfPairsOfCards = [
-           "animalPairNumbers": 10,
-           "vehiclePairNumbers": 14,
-           "fruitPairNumbers": 8,
-           "foodPairNumbers": 10,
-           "flagPairNumbers": 12,
-           "activityPairNumber": 8
-        ]
-
         
-        func checkEmojiCount(forProposedNumberOfPairs numberOfPairs: Int, fromSetOfEmoji setOfEmoji: [String]) ->Int {
-            numberOfPairs > setOfEmoji.count ? setOfEmoji.count : numberOfPairs
-        }
-        
-        animalTheme = Theme(themeName: "Animals",
-                            forEmojiSet: animalemojis,
-                            withColour: "red") {
-            checkEmojiCount(forProposedNumberOfPairs: themeNumbersOfPairsOfCards["animalPairNumbers"] ?? 0, fromSetOfEmoji: animalemojis)
-        }
-        themes.append(animalTheme)
-        
-        vehicleTheme = Theme(themeName: "Vehicles",
-                             forEmojiSet: vehicleEmojis,
-                             withColour: "yellow") {
-            checkEmojiCount(forProposedNumberOfPairs: themeNumbersOfPairsOfCards["vehiclePairNumbers"] ?? 0, fromSetOfEmoji: vehicleEmojis)
-        }
-        themes.append(vehicleTheme)
-        
-        fruitTheme = Theme(themeName: "Fruit",
-                           forEmojiSet: fruitEmojis,
-                           withColour: "green") {
-            checkEmojiCount(forProposedNumberOfPairs: themeNumbersOfPairsOfCards["fruitPairNumbers"] ?? 0, fromSetOfEmoji: fruitEmojis)
-        }
-        themes.append(fruitTheme)
-        
-        foodTheme = Theme(themeName: "Food",
-                          forEmojiSet: foodEmojis,
-                          withColour: "blue") {
-            checkEmojiCount(forProposedNumberOfPairs: themeNumbersOfPairsOfCards["foodPairNumbers"] ?? 0, fromSetOfEmoji: foodEmojis)
-        }
-        themes.append(foodTheme)
-        
-        flagTheme = Theme(themeName: "Flags",
-                          forEmojiSet: flagEmojis,
-                          withColour: "orange") {
-            checkEmojiCount(forProposedNumberOfPairs: themeNumbersOfPairsOfCards["flagPairNumbers"] ?? 0, fromSetOfEmoji: flagEmojis)
-        }
-        themes.append(flagTheme)
-        
-        activityTheme = Theme(themeName: "Activities",
-                              forEmojiSet: activityEmojis,
-                              withColour: "brown") {
-            checkEmojiCount(forProposedNumberOfPairs: themeNumbersOfPairsOfCards["activityPairNumber"] ?? 0, fromSetOfEmoji: activityEmojis)
-        }
-        themes.append(activityTheme)
-        
-        currentTheme = animalTheme
-    }
-    
-
-
-    
-    static func createMemoryGame(forTheme theme: Theme) -> MemoryGame<String> {
-        
-        return MemoryGame<String>(numberOfPairsOfCards: theme.numberOfPairsOfCards) { pairIndex in theme.emojiSet[pairIndex] }
+        return MemoryGame<String>(numberOfPairsOfCards: 9) { pairIndex in emojis[pairIndex] }
     }
 
-    func getColourForCurrentTheme() -> Color {
-        let colourString = currentTheme.colour
-        switch colourString {
-        case "red" : return Color.red
-        case "yellow": return Color.yellow
-        case "green": return Color.green
-        case "blue": return Color.blue
-        case "orange": return Color.orange
-        case "brown": return Color.brown
-        default: return Color.blue
-        }
-    }
     
-    @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame(forTheme: Theme())
+    @Published private var model = EmojiMemoryGame.createMemoryGame()
         
-    var cards: Array<MemoryGame<String>.Card> {
+    var cards: Array<Card> {
         model.cards
     }
     
-    var score: Int {
-        model.score
-    }
 
     
     // MARK: - Intent(s)
     
-    func choose(_ card: MemoryGame<String>.Card) {
+    func choose(_ card: Card) {
         objectWillChange.send()
         model.choose(card)
     }
-    
-    func changeTheme()  {
-        let randomInt: Int = Int.random(in: 0..<themes.count)
-        objectWillChange.send()
-        currentTheme = themes[randomInt]
-        model = EmojiMemoryGame.createMemoryGame(forTheme: currentTheme)
-    
-    }
+
     
 }
--- MemorizeTakeTwo/MemoryGame.swift
+++ MemorizeTakeTwo/MemoryGame.swift
@@ -10,9 +10,12 @@ import UIKit
 
 struct MemoryGame<CardContent> where CardContent: Equatable{
     private(set) var cards: Array<Card>
-    var score = 0
-    private var alreadySeen: [Int] = []
-    private var indexOfTheOneAndOnlyFaceUpCard: Int?
+    
+    private var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter({ cards[$0].isFaceUp }).oneAndOnly }
+        set { cards.indices.forEach { cards[$0].isFaceUp = ($0 == newValue) }
+        }
+    }
     
     
     mutating func choose(_ card: Card) {
@@ -24,23 +27,12 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
                 if cards[chosenIndex].content == cards[potentialMatchIndex].content {
                     cards[chosenIndex].isMatched = true
                     cards[potentialMatchIndex].isMatched = true
-                    score += 2
-                } else if !alreadySeen.contains(card.id){
-                    
-                    alreadySeen.append(card.id)
-                    print(alreadySeen)
-                } else {
-                    score -= 1
-                    
                 }
-                indexOfTheOneAndOnlyFaceUpCard = nil
-            } else {
-                for index in cards.indices {
-                    cards[index].isFaceUp = false
-                }
-                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                cards[chosenIndex].isFaceUp = true
             }
-            cards[chosenIndex].isFaceUp.toggle()
+            else {
+               indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+            }
         }
     }
     
@@ -48,7 +40,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
     
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
-        cards = Array<Card>()
+        cards = []
         // add numbersofPairsOfCards x 2 to cards array
         for pairIndex in 0..<numberOfPairsOfCards {
             let content = createCardContent(pairIndex)
@@ -59,42 +51,20 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
     }
     
     struct Card: Identifiable {
-        var isFaceUp: Bool = false
-        var isMatched: Bool = false
-        var content: CardContent
-        var id: Int
+        var isFaceUp = true
+        var isMatched = false
+        let content: CardContent
+        let id: Int
     }
 }
 
-struct Theme {
-    var name: String = "Animals"
-    private(set) var numberOfPairsOfCards: Int = 10
-    private(set) var emojiSet: [String] =  ["🐝", "🐛", "🐜", "🪰", "🦋", "🐌", "🐥", "🦞", "🐒", "🦓", "🦏", "🐓", "🦎", "🐪", "🦒", "🦬", "🐂", "🐎", "🐓", "🦨", "🦫", "🦜", "🐁", "🐿"]
-
-    var colour: String = "red"
-
-    init(){
-        
-    }
-    init(themeName: String, forEmojiSet themeEmojiSet: [String], withColour themeColour: String, getNumberOfCards: () ->Int) {
-        name = themeName
-        numberOfPairsOfCards = getNumberOfCards()
-        var emojiSelection =  Array<String>()
-        if themeEmojiSet.count != numberOfPairsOfCards {
-            for _ in  0..<numberOfPairsOfCards {
-                var emojiToAppend: String? = themeEmojiSet.randomElement()
-                while(emojiToAppend == nil || emojiSelection.contains(emojiToAppend!)) {
-                    emojiToAppend = themeEmojiSet.randomElement()
-                }
-                emojiSelection.append(emojiToAppend!)
-            }
-            emojiSet = emojiSelection
+extension Array {
+    var oneAndOnly: Element? {
+        if count == 1 {
+            return first
         } else {
-            emojiSet = themeEmojiSet
+            return nil
         }
-        colour = themeColour
-   }
-    
+    }
 
-    
 }
